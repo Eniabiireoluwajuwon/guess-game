2b6dab3a1eb22a66f51f63d22efe029e0627a902
ENIABIIRE OLUWAJUWON DAMILOLA <133174127+MODE6328@users.noreply.github.com>
2023-05-14 14:13:22 +0100

    Add files via upload

---
+++ guessing game.py
@@ -0,0 +1,17 @@
+import random
+number = random.randint(1, 10)
+secret_number = number
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess_count +=1
+    guess =int(input('guess: '))
+    if guess == secret_number:
+        print('YOU WON!')
+        break
+    elif guess >= number:
+        print('Number too large')
+    elif guess < number:
+        print('Try Again')
+else:
+    print('SORRY YOU FAILED!')
